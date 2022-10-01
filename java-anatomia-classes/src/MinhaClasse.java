f40d740093f12665c3b22483cd488bbb88f00906
allarchas <all.archas@gmail.com>
2022-10-01 09:24:56 -0300

    conhecendo as classes

---
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -0,0 +1,19 @@
+public class MinhaClasse {
+    
+    public static void main (String [] args){
+
+        //Método de output para imprimir uma linha
+        System.out.println("Olá mundo!");
+
+        //Variáveis com letras minusculas podem ser alteradas posteriormente
+        int ano = 2021;
+
+        ano = 2022;
+        
+        //Variáveis do tipo final e com letras maísculas não podem ser alteradas posteriormente
+        final String BR = "Brasil";
+
+
+    }
+
+}
